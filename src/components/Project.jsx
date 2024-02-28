3d51726a23942c8ab683b786765e56692f0b06fb
brodrigues0ll <bernardorg99@hotmail.com>
2024-02-27 21:28:15 -0300

    Projects Section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ import { Link } from "lucide-react";
 
 const Project = ({ title, desc, techStack }) => {
   return (
-    <div className="md:w-[375px] mx-6 mb-10 font-[poppins] flex flex-col rounded-3xl overflow-hidden shadow-2xl shadow-neutral-400 dark:shadow-neutral-900">
+    <div className="md:w-[385px] mx-6 mb-10 font-[poppins] flex flex-col rounded-3xl overflow-hidden shadow-2xl shadow-neutral-400 dark:shadow-neutral-900">
       <div
         style={{
           backgroundImage: `url("https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=1380&t=st=1709075919~exp=1709076519~hmac=c9d9fbbb34005e544cd147f04e8518448d54f3fb4bb950bd51d2abf1fffc902c")`,
@@ -11,8 +11,8 @@ const Project = ({ title, desc, techStack }) => {
         className="h-56 bg-cover bg-center bg-no-repeat"
       />
 
-      <div className="flex flex-col gap-5 pt-16 py-10 px-6 dark:bg-dark-menubg">
-        <h1 className="text-3xl font-medium text-zinc-500 dark:text-zinc-200">
+      <div className="flex flex-col gap-5 pt-16 py-10 px-4 dark:bg-dark-menubg">
+        <h1 className="text-2xl font-medium text-zinc-500 dark:text-zinc-200">
           Project Tile goes here
         </h1>
 
