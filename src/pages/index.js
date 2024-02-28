58274b8bb0350549ddf9d8c61ac63fa9e7de80d0
brodrigues0ll <bernardorg99@hotmail.com>
2024-02-27 22:22:09 -0300

    Testing Preload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { useEffect } from "react";
 import profilePhoto from "../../public/assets/header/profilePhoto.png";
 import cssIcon from "../../public/assets/cssIcon.svg";
 import gitIcon from "../../public/assets/gitIcon.svg";
@@ -13,6 +13,28 @@ import Project from "@/components/Project";
 
 export default function Home() {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    // Preload the images for faster loading
+    const images = [
+      profilePhoto,
+      cssIcon,
+      gitIcon,
+      htmlIcon,
+      jsIcon,
+      reactIcon,
+      tailwindIcon,
+      vscodeIcon,
+    ];
+    images.forEach((image) => {
+      const link = document.createElement("link");
+      link.rel = "preload";
+      link.href = image.src;
+      link.as = "image";
+      document.head.appendChild(link);
+    });
+  }, []);
+
   return (
     <div className="justify-between items-center md:p-10 bg-light-bg dark:bg-dark-bg transition-all duration-500 ease-in-out">
       {/* Presentation Section */}
@@ -35,7 +57,7 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Tech Stach Section */}
+      {/* Tech Stack Section */}
       <section className="pb-20">
         <h1 className="text-light-solidHeading dark:text-dark-solidHeading text-3xl font-bold font-['Poppins'] leading-tight pl-5 pr-5 mb-2 text-center">
           {t("techTitle")}
@@ -47,18 +69,18 @@ export default function Home() {
 
         <div className="flex justify-center">
           <div className="tech-stack__icons gap-10 grid grid-cols-2 justify-center items-center lg:grid lg:grid-cols-6">
-            <Image src={htmlIcon} alt="HTML" />
-            <Image src={cssIcon} alt="CSS" />
-            <Image src={jsIcon} alt="JavaScript" />
-            <Image src={reactIcon} alt="React" />
+            <img src={htmlIcon.src} alt="HTML" />
+            <img src={cssIcon.src} alt="CSS" />
+            <img src={jsIcon.src} alt="JavaScript" />
+            <img src={reactIcon.src} alt="React" />
             <i className="tf tf-nextjs text-zinc-800 dark:text-zinc-200 text-[100px] text-center tech-stack__icon" />
             <i className="tf tf-nodejs-b text-[#529F41] text-[80px] text-center tech-stack__icon" />
             <i className="tf tf-mysql text-[#04546C] text-[100px] text-center tech-stack__icon" />
-            <Image src={tailwindIcon} alt="Tailwind" />
+            <img src={tailwindIcon.src} alt="Tailwind" />
             <i className="tf tf-mongodb-b text-[#529F41] text-[80px] text-center tech-stack__icon" />
-            <Image src={vscodeIcon} alt="VSCode" />
+            <img src={vscodeIcon.src} alt="VSCode" />
             <i className="tf tf-github text-zinc-800 dark:text-zinc-200 text-[80px] text-center tech-stack__icon" />
-            <Image src={gitIcon} alt="Git" />
+            <img src={gitIcon.src} alt="Git" />
           </div>
         </div>
       </section>
